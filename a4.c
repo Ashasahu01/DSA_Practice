e5f2b53a2f5a7c5561541b996904cf3a12df6a39
Ashasahu01 <as20252651013@gmail.com>
2026-02-05 21:31:45 +0530

    implement binary search

---
+++ a4.c
@@ -0,0 +1,69 @@
+
+#include<stdio.h>
+
+int first_occ(int A[],int l,int r,int X){
+        int res = -1;
+        while(l <= r){
+
+                int mid = (l+r)/2;
+
+                if(X == A[mid]){
+                        res = mid;
+                        r = mid -1; // search from l to mid - 1, left of mid element
+                }
+                else if(X < A[mid]){
+                        r = mid -1;
+                }
+                else{
+                        l = mid +1;
+                 }
+                }
+                return res;
+        }
+
+int last_occ(int A[], int l, int r, int X){
+        int res = -1;
+        while(l <= r){
+
+                int mid = (l+r)/2;
+
+                if(X == A[mid]){
+                        res = mid;
+                        l = mid + 1;
+                }
+                else if(X > A[mid]){
+                        l = mid + 1;
+                }
+                else{
+                        r = mid - 1;
+                }
+        }
+        return res;
+}
+
+int main(){
+        int n, X;
+        printf("Size of Array: ");
+        scanf("%d", &n);
+
+        int A[n];
+        printf("Enter sorted array of size %d ", n);
+        for(int i=0; i<n; i++){
+                scanf("%d", &A[i]);
+        }
+        printf("Key value : ");
+        scanf("%d", &X);
+
+        int first = first_occ(A, 0, n-1, X);
+        int last = last_occ(A, 0, n-1, X);
+
+        if(first == -1)
+                printf("Element not found.");
+        else{
+                int count = last - first + 1;
+                printf("Occ(%d) : %d", X, count);
+        }
+
+        return 0;
+
+}
